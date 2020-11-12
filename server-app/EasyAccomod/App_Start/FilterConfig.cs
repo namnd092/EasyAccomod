5431bb960d0ec51adb246653ed89ce8b27b1b020
mcd_oblivion <mcd.oblivion@gmail.com>
2020-11-12 11:22:40 +0700

    Config for run server app in VSCode

--- server-app/EasyAccomod/App_Start/FilterConfig.cs
+++ server-app/EasyAccomod/App_Start/FilterConfig.cs
@@ -8,8 +8,6 @@ namespace EasyAccomod
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new AuthorizeAttribute());
-            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
