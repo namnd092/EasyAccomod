9d64626acc6a98518aa5d10572bb8ed3c494ced5
mcd_oblivion <mcd.oblivion@gmail.com>
2020-11-21 10:30:14 +0700

    Create Accommodation types in Database

---
+++ server-app/EasyAccomod/Migrations/202011210324527_CreateAccommodationTypesInDb.cs
@@ -0,0 +1,21 @@
+﻿namespace EasyAccomod.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class CreateAccommodationTypesInDb : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"INSERT INTO [dbo].[AccommodationTypes] ([Name]) VALUES (N'Phòng trọ')
+INSERT INTO [dbo].[AccommodationTypes] ([Name]) VALUES (N'Chung cư mini')
+INSERT INTO [dbo].[AccommodationTypes] ([Name]) VALUES (N'Nhà nguyên căn')
+INSERT INTO [dbo].[AccommodationTypes] ([Name]) VALUES (N'Chung cư nguyên căn')
+");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
