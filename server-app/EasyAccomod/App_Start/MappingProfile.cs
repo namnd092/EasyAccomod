9e6ad39160411175f9b22960d42c5e812fb5f422
mcd_oblivion <mcd.oblivion@gmail.com>
2020-11-26 01:07:41 +0700

    Add Accommodation DTO

--- server-app/EasyAccomod/App_Start/MappingProfile.cs
+++ server-app/EasyAccomod/App_Start/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using EasyAccomod.Dtos;
+using EasyAccomod.Models;
 using EasyAccomod.Models.AddressModel;
 
 namespace Vidly.App_Start
@@ -13,10 +14,25 @@ namespace Vidly.App_Start
             Mapper.CreateMap<Province, ProvinceDto>();
             Mapper.CreateMap<Ward, WardDto>();
 
+            Mapper.CreateMap<Accommodation, AccommodationDto>();
+            Mapper.CreateMap<Address, AddressDto>();
+            Mapper.CreateMap<AccommodationType, AccommodationTypeDto>();
+            Mapper.CreateMap<AccommodationPaymentType, AccommodationTypeDto>();
+            Mapper.CreateMap<RoomAreaRange, RoomAreaRangeDto>();
+            Mapper.CreateMap<KitchenType, KitchenTypeDto>();
+            Mapper.CreateMap<Owner, OwnerDto>();
+
             // Dto to Domain
             Mapper.CreateMap<DistrictDto, District>();
             Mapper.CreateMap<ProvinceDto, Province>();
             Mapper.CreateMap<WardDto, Ward>();
+
+            Mapper.CreateMap<AccommodationDto, Accommodation>()
+                .ForMember(a => a.Id, opt => opt.Ignore());
+            Mapper.CreateMap<AddressDto, Address>()
+                .ForMember(a => a.Id, opt => opt.Ignore());
+            Mapper.CreateMap<OwnerDto, Owner>()
+                .ForMember(o => o.Id, opt => opt.Ignore());
         }
     }
 }
---
+++ server-app/EasyAccomod/Dtos/AccommodationDto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using EasyAccomod.Models;
+using EasyAccomod.Models.AddressModel;
+
+namespace EasyAccomod.Dtos
+{
+    public class AccommodationDto
+    {
+        public int Id { get; set; }
+
+        public AddressDto Address { get; set; }
+
+        [Required]
+        public int AddressId { get; set; }
+
+        public AccommodationTypeDto AccommodationType { get; set; }
+
+        [Required]
+        public int AccommodationTypeId { get; set; }
+
+        [Required] public int RoomQuantity { get; set; }
+
+        public AccommodationPaymentTypeDto PaymentType { get; set; }
+
+        public byte PaymentTypeId { get; set; }
+
+        public string Price { get; set; }
+
+        public RoomAreaRangeDto RoomAreaRange { get; set; }
+
+        public byte RoomAreaRangeId { get; set; }
+
+        public bool LiveWithOwner { get; set; }
+
+        [Required] public bool HaveClosedBathroom { get; set; }
+
+        [Required] public bool HaveWaterHeater { get; set; }
+
+        public KitchenTypeDto KitchenType { get; set; }
+
+        [Required]
+        public byte KitchenTypeId { get; set; }
+
+        [Required] public bool HaveAirConditioner { get; set; }
+
+        [Required] public bool HaveBalcony { get; set; }
+
+        [Required] public int ElectricityPrice { get; set; }
+
+        [Required] public int WaterPrice { get; set; }
+
+        public string RoomOptions { get; set; }
+
+        public OwnerDto Owner { get; set; }
+
+        [Required]
+        public int OwnerId { get; set; }
+
+        [Required] public string Status { get; set; }
+    }
+}
